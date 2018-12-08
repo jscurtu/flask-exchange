a2dddb3e03c14c488a90ee63df4858d832d4e841
Jason Scurtu <scurtu@mail.de>
2018-12-08 17:14:58 +0100

    bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/jscurtu/flask-exchange'
 EMAIL = 'jscurtu@gmail.com'
 AUTHOR = 'Jason Scurtu'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
